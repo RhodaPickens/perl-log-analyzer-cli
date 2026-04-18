07d58daec09076750932f7fe71a6d9afc8a83026
Rhoda Pickens <rhoda@Mac.lan>
2026-04-18 01:08:27 -0500

    Added error and warning detection

--- analyzer.pl
+++ analyzer.pl
@@ -12,6 +12,14 @@ open(INPUT, $logfile) or die("Log file $logfile not found.\n");
 # loop through each line
 while(my $line = <INPUT>) {
     print $line;
+    # Detect Error and Warning lines
+    if($line =~ /(ERROR:)/) {
+        print "Matched: '$1'\n";
+    }
+    if($line =~ /(WARNING:)/) {
+        print "Matched: '$1'\n";
+    }
+    
 }
 
 close(INPUT);
